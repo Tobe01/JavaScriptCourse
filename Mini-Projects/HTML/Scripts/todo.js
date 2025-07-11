9bac5d6e013263f77b5820d35ab194c8b8b7c5ed
Tobechi Duru <tobechi.duru001@gmail.com>
2025-07-11 12:58:54 -0700

    started a todoApp project

--- Mini-Projects/HTML/Scripts/todo.js
+++ Mini-Projects/HTML/Scripts/todo.js
@@ -1,16 +1,19 @@
 let input = document.querySelector('.js-input-field');
 let btn = document.querySelector('.js-button');
 let display = document.querySelector('.js-display');
-let todoItem = [];
-let itemHtml = ''
+let todoItem = ['make dinner', 'wash dishes'];
+
 
 btn.addEventListener('click', () => {
+   let date = document.querySelector('.js-date-field');
+   let dateValue = date.value;
     let inputElement = input.value;
       if (inputElement){
-       todoItem.push(inputElement);
-       display.innerHTML = todoItem;
-
-       input.value = '';
+       todoItem.forEach((item) =>{
+        let html = `<p>${inputElement} ${dateValue}<button class="js-delete-btn" data-index="${index}>Delete</button></p>`;
+        display.innerHTML += html;
+        input.value = '';
+       })
   }
 })
 
